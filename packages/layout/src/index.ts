ea03cd191f17d1342e8d7c5aa2ac8a9ce4d88a01
Dan Lynch <pyramation@gmail.com>
2026-08-06 19:17:57 +0000

    chore(layout): eslint import ordering for light-map exports

--- packages/layout/src/index.ts
+++ packages/layout/src/index.ts
@@ -33,8 +33,8 @@ export { getPresetNames, presets, resolveLayout } from './presets';
 // Light-map helpers (physical correction layer) + auto-map heuristics
 export {
   autoMap,
-  type AutoMapStrategy,
   autoMapStrategies,
+  type AutoMapStrategy,
   availableStrategies,
   identityMap,
   isIdentityMap,
